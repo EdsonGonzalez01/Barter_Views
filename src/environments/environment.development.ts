f9505c28e0c74e3ac4d95faea71d0792dff8a983
Edson Glz <edzziton@hotmail.com>
2023-12-03 19:27:00 -0600

    Editing env variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'http://localhost:3000/',
+    apiUrl: 'https://trueque-barter.onrender.com/',
     googleClient: '264879683267-spjp2jpn7vqorbodu9n6s7mjgf24rn7l.apps.googleusercontent.com'
     //googleClient: '216316667892-a8s3nqf1ihn7cpfqjsf0rqm8qsjv5d6r.apps.googleusercontent.com'
 };
